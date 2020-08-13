904b098689fdd8273135099357bf3c0d1725003b
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-13 16:49:41 -0600

    half done

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+class Player:
+    def __init__(self, current_room):
+        self.current_room: Room = current_room
